e5f374cc62de1c36ee7ae720323f4736093e315e
Murat <dautovmurat5@gmail.com>
2020-12-05 06:52:09 +0300

    fix main page

--- script/example-todo.js
+++ script/example-todo.js
@@ -1,41 +1,18 @@
 var todo = document.querySelector(".todo");
-var columns = todo.querySelectorAll(".todo-column");
-var tasks = new Array(columns.length);
-for (let i = 0; i < columns.length; i++) {
-    const column = columns[i];
-    var taskContainer = column.querySelector(".todo-tasks-container");
-    tasks[i] = taskContainer.childNodes;
-    for (let j = 0; j < tasks[i].length; j++) {
-        var task = tasks[i][j];
-        task.onmousedown = function (event) {
-            let shiftX = event.clientX - task.getBoundingClientRect().left;
-            let shiftY = event.clientY - task.getBoundingClientRect().top;
+var addColumn = todo.querySelector(".todo-add-column");
+var columns = [];
+// var tasks = new Array(columns.length);
+//todo init with start board
+var columnTemplate = todo.querySelector(".todo-column-template");
+var addColumnForm = todo.querySelector(".todo-column-form");
 
-            var taskCopy = task.cloneNode(true);
-            taskCopy = taskContainer.insertBefore(taskCopy, task);
-            task.classList.add("todo-task-moving");
-            taskCopy.classList.add("todo-task-transperent");
-
-            document.body.append(task);
-
-            moveAt(event.pageX, event.pageY);
-            function moveAt(pageX, pageY) {
-                task.style.left = pageX - shiftX + 'px';
-                task.style.top = pageY - shiftY + 'px';
-            }
-            function onMouseMove(event) {
-                moveAt(event.pageX, event.pageY);
-            }
-            document.addEventListener('mousemove', onMouseMove);
-            task.onmouseup = function () {
-                document.removeEventListener('mousemove', onMouseMove);
-                task.onmouseup = null;
-                task = taskContainer.insertBefore(task, taskCopy);
-                taskCopy.remove();
-            };
-        }
-        task.ondragstart = function () {
-            return false;
-        };
-    }
+addColumnForm.onsubmit = function () {
+    let baseHeader = "Column header";
+    var clone = columnTemplate.content.cloneNode(true);
+    clone = clone.querySelector(".todo-column");
+    clone.querySelector(".todo-column-header").textContent = baseHeader;
+    clone = todo.insertBefore(clone, addColumn);
+    columns.push(clone);
+    todo.scrollLeft = todo.scrollWidth;
+    return false;
 }
